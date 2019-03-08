de7dd0b59c7bce02f481868a19e35cfaf8819d6a
Lukkario <michallukianski.0@gmail.com>
2019-03-08 09:06:05 +0100

    Taks 1 feature 1 fix 1

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world ale lepsze");
+        System.out.println("Hello world ale lepsze lepsze");
     }
 }
